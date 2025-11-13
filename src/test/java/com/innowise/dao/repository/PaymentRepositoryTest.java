ad958af03a9b41e44155ea9d4f39c692ffef7076
kai <kirillsavenko1705@gmail.com>
2025-11-14 00:49:17 +0100

    fixed issue in test

--- src/test/java/com/innowise/dao/repository/PaymentRepositoryTest.java
+++ src/test/java/com/innowise/dao/repository/PaymentRepositoryTest.java
@@ -5,7 +5,8 @@ import com.innowise.model.entity.Payment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,7 +15,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataMongoTest
+@SpringBootTest
+@ActiveProfiles("ci")
 class PaymentRepositoryTest {
 
     @Autowired
